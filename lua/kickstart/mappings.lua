ada2fbc2c3189fe8a7950805b751beef0dd810d5
Franky Chan <franky@datch.io>
2023-02-19 23:43:12 +1300

    new

--- lua/kickstart/mappings.lua
+++ lua/kickstart/mappings.lua
@@ -28,8 +28,8 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 -- New tab
 keymap.set('n', 'te', ':tabedit')
 -- Split window
-keymap.set('n', 's-', ':split<Return><C-w>w')
-keymap.set('n', 's|', ':vsplit<Return><C-w>w')
+keymap.set('n', '<C-w>-', ':split<Return><C-w>w')
+keymap.set('n', '<C-w>|', ':vsplit<Return><C-w>w')
 -- Move between windows
 keymap.set('n', '<Space>', '<C-w>w')
 
@@ -38,3 +38,12 @@ keymap.set('n', '<C-w><left>', '<C-w><')
 keymap.set('n', '<C-w><right>', '<C-w>>')
 keymap.set('n', '<C-w><up>', '<C-w>+')
 keymap.set('n', '<C-w><down>', '<C-w>-')
+
+--  auto compleete
+keymap.set('i', '"', '""<left>', {noremap = true})
+keymap.set('i', "'", "''<left>", { noremap = true })
+keymap.set('i', '(', '()<left>', { noremap = true })
+keymap.set('i', '[', '[]<left>', { noremap = true })
+keymap.set('i', '{', '{}<left>', { noremap = true })
+keymap.set('i', '{<CR>', '{<CR>}<ESC>O', { noremap = true })
+keymap.set('i', '{;', '{<CR>};<ESC>O', { noremap = true })
