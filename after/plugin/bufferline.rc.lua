e8bf00195a5ac5b128638d7c3738831c6939b270
Franky Chan <franky@datch.io>
2023-02-21 16:13:03 +1300

    Edit lualine after plugin

--- after/plugin/bufferline.rc.lua
+++ after/plugin/bufferline.rc.lua
@@ -3,7 +3,43 @@ if (not status) then return end
 
 vim.opt.termguicolors = true
 
-bufferline.setup({})
+bufferline.setup({
+  options = {
+    mode = "tabs",
+    -- separator_style = 'slant',
+    always_show_bufferline = true,
+    show_buffer_close_icons = false,
+    show_close_icon = false,
+    color_icons = true
+  },
+  highlights = {
+    separator = {
+      fg = '#30354e',
+      -- Unselected buffer tab separator background
+      bg ='#1d202f',
+    },
+    separator_selected = {
+      fg = '#1d202f',
+      bg ='#1d202f',
+    },
+    background = {
+      fg = '#657b83',
+      -- Unselected buffer tab background
+      bg ='#1d202f',
+    },
+    buffer_selected = {
+      -- text color
+      fg = '#fdf6e3',
+      -- tab background color
+      bg = '#434a6d',
+      bold = true,
+    },
+    fill = {
+      -- Track background
+      -- bg = '#30354e'
+    }
+  },
+})
 
 vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
 vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
--- after/plugin/null-ls.rc.lua
+++ after/plugin/null-ls.rc.lua
@@ -1,45 +1,88 @@
--- Null ls
+-- -- Null ls
+-- local lsp_formatting = function(bufnr)
+--     vim.lsp.buf.format({
+--         filter = function(client)
+--             -- apply whatever logic you want (in this example, we'll only use null-ls)
+--             return client.name == "null-ls"
+--         end,
+--         bufnr = bufnr,
+--     })
+-- end
+--
+-- -- if you want to set up formatting on save, you can use this as a callback
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+--
+-- -- add to your shared on_attach callback
+-- local on_attach = function(client, bufnr)
+--     if client.supports_method("textDocument/formatting") then
+--         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--         vim.api.nvim_create_autocmd("BufWritePre", {
+--             group = augroup,
+--             buffer = bufnr,
+--             callback = function()
+--                 lsp_formatting(bufnr)
+--             end,
+--         })
+--     end
+-- end
+--
+-- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- require("null-ls").setup({
+--     -- you can reuse a shared lspconfig on_attach callback here
+--     on_attach = function(client, bufnr)
+--         if client.supports_method("textDocument/formatting") then
+--             vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+--             vim.api.nvim_create_autocmd("BufWritePre", {
+--                 group = augroup,
+--                 buffer = bufnr,
+--                 callback = function()
+--                     -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+--                     vim.lsp.buf.format({ bufnr = bufnr }) 
+--                 end,
+--             })
+--         end
+--     end,
+-- })
+local status, null_ls = pcall(require, "null-ls")
+if (not status) then return end
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 local lsp_formatting = function(bufnr)
-    vim.lsp.buf.format({
-        filter = function(client)
-            -- apply whatever logic you want (in this example, we'll only use null-ls)
-            return client.name == "null-ls"
-        end,
-        bufnr = bufnr,
-    })
-end
-
--- if you want to set up formatting on save, you can use this as a callback
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
--- add to your shared on_attach callback
-local on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                lsp_formatting(bufnr)
-            end,
-        })
-    end
-end
-
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-require("null-ls").setup({
-    -- you can reuse a shared lspconfig on_attach callback here
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    vim.lsp.buf.format({ bufnr = bufnr }) 
-                end,
-            })
-        end
+  vim.lsp.buf.format({
+    filter = function(client)
+      return client.name == "null-ls"
     end,
-})
+    bufnr = bufnr,
+  })
+end
+
+null_ls.setup {
+  sources = {
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.diagnostics.eslint_d.with({
+      diagnostics_format = '[eslint] #{m}\n(#{c})'
+    }),
+    null_ls.builtins.diagnostics.fish
+  },
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          lsp_formatting(bufnr)
+        end,
+      })
+    end
+  end
+}
+
+vim.api.nvim_create_user_command(
+  'DisableLspFormatting',
+  function()
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
+  end,
+  { nargs = 0 }
+)
