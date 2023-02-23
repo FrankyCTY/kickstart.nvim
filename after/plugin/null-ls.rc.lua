a7403f72c4b35cef4926712b9e536fb786269400
Franky Chan <franky@datch.io>
2023-02-24 01:56:09 +1300

    Add null ls eslint code action

--- after/plugin/null-ls.rc.lua
+++ after/plugin/null-ls.rc.lua
@@ -37,38 +37,43 @@
 --                 buffer = bufnr,
 --                 callback = function()
 --                     -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
---                     vim.lsp.buf.format({ bufnr = bufnr }) 
+--                     vim.lsp.buf.format({ bufnr = bufnr })
 --                 end,
 --             })
 --         end
 --     end,
 -- })
-local status, null_ls = pcall(require, "null-ls")
-if (not status) then return end
+local status, null_ls = pcall(require, 'null-ls')
+if not status then
+  return
+end
 
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local augroup = vim.api.nvim_create_augroup('LspFormatting', {})
 
 local lsp_formatting = function(bufnr)
-  vim.lsp.buf.format({
+  vim.lsp.buf.format {
     filter = function(client)
-      return client.name == "null-ls"
+      return client.name == 'null-ls'
     end,
     bufnr = bufnr,
-  })
+  }
 end
 
 null_ls.setup {
   sources = {
     null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.diagnostics.eslint_d.with({
-      diagnostics_format = '[eslint] #{m}\n(#{c})'
-    }),
-    null_ls.builtins.diagnostics.fish
+    null_ls.builtins.formatting.stylua,
+    null_ls.builtins.diagnostics.eslint_d.with {
+      diagnostics_format = '[eslint] #{m}\n(#{c})',
+    },
+    null_ls.builtins.diagnostics.hadolint,
+    null_ls.builtins.diagnostics.markdownlint,
+    null_ls.builtins.code_actions.eslint_d,
   },
   on_attach = function(client, bufnr)
-    if client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
+    if client.supports_method 'textDocument/formatting' then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd('BufWritePre', {
         group = augroup,
         buffer = bufnr,
         callback = function()
@@ -76,13 +81,21 @@ null_ls.setup {
         end,
       })
     end
-  end
+  end,
 }
 
-vim.api.nvim_create_user_command(
-  'DisableLspFormatting',
-  function()
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
-  end,
-  { nargs = 0 }
-)
+vim.api.nvim_create_user_command('DisableLspFormatting', function()
+  vim.api.nvim_clear_autocmds { group = augroup, buffer = 0 }
+end, { nargs = 0 })
+
+null_ls.builtins.code_actions.eslint.with {
+  name = 'eslint_d',
+  command = 'eslint_d',
+  meta = {
+    url = 'https://github.com/mantoni/eslint_d.js',
+    description = 'Injects actions to fix ESLint issues or ignore broken rules. Like ESLint, but faster.',
+    notes = {
+      'Once spawned, the server will continue to run in the background. This is normal and not related to null-ls. You can stop it by running `eslint_d stop` from the command line.',
+    },
+  },
+}
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,62 +3,64 @@
 --
 -- See the kickstart.nvim README for more information
 
-vim.g.gitblame_enabled = 0;
+vim.g.gitblame_enabled = 0
 
 return {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    dependencies = {"nvim-tree/nvim-web-devicons" -- not strictly required, but recommended
-    },
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+  },
   {
-   'folke/tokyonight.nvim'
+    'folke/tokyonight.nvim',
   },
   -- For the file icons
   {
-    'nvim-tree/nvim-web-devicons'
+    'nvim-tree/nvim-web-devicons',
   },
   {
-    'jose-elias-alvarez/null-ls.nvim'
+    'jose-elias-alvarez/null-ls.nvim',
   },
   {
-    'MunifTanjim/prettier.nvim'
+    'MunifTanjim/prettier.nvim',
   },
   {
-  "windwp/nvim-autopairs",
-  config = function()
-    require("nvim-autopairs").setup {}
-  end,
+    'windwp/nvim-autopairs',
+    config = function()
+      require('nvim-autopairs').setup {}
+    end,
   },
   {
-  'glepnir/dashboard-nvim',
-  event = 'VimEnter',
-  -- config = function()
-  --   require('dashboard').setup {
-  --     -- config
-  --   }
-  -- end,
-  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+    'glepnir/dashboard-nvim',
+    event = 'VimEnter',
+    -- config = function()
+    --   require('dashboard').setup {
+    --     -- config
+    --   }
+    -- end,
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
   },
   {
-    'akinsho/bufferline.nvim', dependencies = {'nvim-tree/nvim-web-devicons'}
+    'akinsho/bufferline.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
   {
-      'glacambre/firenvim',
-      -- Lazy load firenvim
-      -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-      cond = not not vim.g.started_by_firenvim,
-      build = function()
-          require("lazy").load({ plugins = "firenvim", wait = true })
-          vim.fn["firenvim#install"](0)
-      end
+    'glacambre/firenvim',
+    -- Lazy load firenvim
+    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+    cond = not not vim.g.started_by_firenvim,
+    build = function()
+      require('lazy').load { plugins = 'firenvim', wait = true }
+      vim.fn['firenvim#install'](0)
+    end,
   },
   {
-    'f-person/git-blame.nvim'
+    'f-person/git-blame.nvim',
   },
   {
-    'axelvc/template-string.nvim'
+    'axelvc/template-string.nvim',
   },
   {
-    'princejoogie/dir-telescope.nvim'
-  }
+    'princejoogie/dir-telescope.nvim',
+  },
 }
